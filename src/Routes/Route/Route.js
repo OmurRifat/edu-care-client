9e2e01732af43f73473ddea7707aeb862af4b451
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-10-25 09:58:50 +0600

    header section added

--- src/Routes/Route/Route.js
+++ src/Routes/Route/Route.js
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../../components/Home/Home";
 import Main from "../../layouts/Main";
 
 const routes = createBrowserRouter([
     {
         path: '/',
-        element: <Main></Main>
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+
+            }
+        ]
     },
     {
         path: '*',
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            This is Home section
+            <div className='header-content'>
+                <div className=' text-white text-center'>
+                    <h4 className='text-4xl font-semibold text-white text-center pt-24'>Let's Learn With EDUCARE!</h4>
+                    <p className='mt-4'>We will ensure your quality Education with proper Care.<br></br>And We promised to do that.</p>
+                    <Link to='/' className="btn mt-8 bg-indigo-600 text-white hover:bg-indigo-700">Learn More</Link>
+                </div>
+            </div>
         </div>
     );
 };
